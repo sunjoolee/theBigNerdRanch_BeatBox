c81bb02eefef761e7b33102c955ca6317b5caf8d
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-09-01 16:11:52 +0900

    Ch20 단위 테스트와 오디오 재생
    객체의 상호작용 테스트하기

--- app/src/test/java/silbajuk/ch8/beatbox/SoundViewModelTest.kt
+++ app/src/test/java/silbajuk/ch8/beatbox/SoundViewModelTest.kt
@@ -6,21 +6,36 @@ import org.junit.Before
 
 import org.junit.Assert.*
 import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
 
 class SoundViewModelTest {
 
     private lateinit var sound:Sound
     private lateinit var subject: SoundViewModel
+    private lateinit var beatBox: BeatBox
 
     @Before
     fun setUp() {
+        beatBox = mock(BeatBox::class.java)
+
         sound = Sound("assetPath")
-        subject = SoundViewModel()
+        subject = SoundViewModel(beatBox)
         subject.sound = sound
     }
 
+    //SoundViewModel의 title 속성값이 Sound의 name 속성값과 일치하는지 검사
     @Test
     fun exposesSoundNameAsTitle(){
         MatcherAssert.assertThat(subject.title, `is`(sound.name))
     }
+
+    //SoundViewModel과 BeatBox.play(Sound) 함수가 잘 연동되는지 검사
+    @Test
+    fun callsBeatBoxPlayOnButtonCLicked(){
+        subject.onButtonClicked()
+
+        verify(beatBox).play(sound)
+    }
+
 }
