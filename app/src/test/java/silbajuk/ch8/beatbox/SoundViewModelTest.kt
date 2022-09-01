d251958913bb4d581aec450f014d4b70897da921
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-09-01 15:51:38 +0900

    Ch20 단위 테스트와 오디오 재생
    테스트 설정하기
    테스트 작성하기
    update app's build.gradle to the current stable release of mokito.

--- app/src/test/java/silbajuk/ch8/beatbox/SoundViewModelTest.kt
+++ app/src/test/java/silbajuk/ch8/beatbox/SoundViewModelTest.kt
@@ -1,12 +1,26 @@
 package silbajuk.ch8.beatbox
 
+import org.hamcrest.core.Is.`is`
+import org.hamcrest.MatcherAssert
 import org.junit.Before
 
 import org.junit.Assert.*
+import org.junit.Test
 
 class SoundViewModelTest {
 
+    private lateinit var sound:Sound
+    private lateinit var subject: SoundViewModel
+
     @Before
     fun setUp() {
+        sound = Sound("assetPath")
+        subject = SoundViewModel()
+        subject.sound = sound
+    }
+
+    @Test
+    fun exposesSoundNameAsTitle(){
+        MatcherAssert.assertThat(subject.title, `is`(sound.name))
     }
 }
